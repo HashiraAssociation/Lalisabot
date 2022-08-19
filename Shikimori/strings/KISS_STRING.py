dcf505c117c66c3b5044542599f37a77ee450624
SOME1HING <yashprakash2005@gmail.com>
2022-08-19 19:09:01 +0530

    Update and rename kiss_gif.py to KISS_STRING.py

--- Shikimori/strings/KISS_STRING.py
+++ Shikimori/strings/KISS_STRING.py
@@ -1,4 +1,4 @@
-KISS_GIFS = "https://telegra.ph/file/2790d6fd83efc94f7ed5c.mp4",
+kiss = "https://telegra.ph/file/2790d6fd83efc94f7ed5c.mp4",
 "https://telegra.ph/file/bd3f57dd933de6a44fe38.mp4",
 "https://telegra.ph/file/297de9261a46dfd3a2ef8.mp4",
 "https://telegra.ph/file/6108acfa3737a37cf9d44.mp4",
