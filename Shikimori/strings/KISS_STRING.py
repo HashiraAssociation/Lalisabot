928747156e38a58dedd97e20502132214989f814
SOME1HING <yashprakash2005@gmail.com>
2022-08-20 01:07:25 +0530

    Update KISS_STRING.py

--- Shikimori/strings/KISS_STRING.py
+++ Shikimori/strings/KISS_STRING.py
@@ -5,3 +5,9 @@ kiss = ("https://telegra.ph/file/2790d6fd83efc94f7ed5c.mp4",
         "https://telegra.ph/file/0266411fd5c3915d315fa.mp4"
        )
 
+hug = ("https://telegra.ph/file/31f5bc3d589865bf40e40.mp4",
+      "https://telegra.ph/file/24e35a8540ba13f84dbbf.mp4",
+      "https://telegra.ph/file/4da5ce5b405f3d2073f0f.mp4",
+      "https://telegra.ph/file/e0d0b06b3dff0328aafea.mp4",
+      "https://telegra.ph/file/65534f8025373219ed715.mp4",
+      )
